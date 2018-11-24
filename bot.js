663c99160482025b5f1906d56286b9f7fb72d218
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-24 12:41:06 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,113 @@
+const Discord = require('discord.js');
+const bot16 = new Discord.Client();
+
+bot16.on('ready', () => {
+  console.log('Logged in as S16-17-18')
+});
+
+
+bot16.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot16.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot16.on('ready', () => {
+  let ch = bot16.guilds.get("507972785650401301").channels.find(c => c.id == "515838588299247626");
+  setInterval(function() {
+    ch.send("16th Lover")
+  }, 900);
+});
+
+
+bot16.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f16"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot16.login(process.env.FB6);
+////////////////////////
+////////////////////////
+const bot17 = new Discord.Client();
+bot17.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot17.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot17.on('ready', () => {
+  let ch = bot17.guilds.get("507972785650401301").channels.find(c => c.id == "515838588299247626");
+  setInterval(function() {
+    ch.send("17th Lover")
+  }, 900);
+});
+
+
+bot17.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f17"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot17.login(process.env.FB17);
+////////////////////////
+////////////////////////
+const bot18 = new Discord.Client();
+bot18.on('message', message => {
+  if(message.content.startsWith('f-d')){
+    message.channel.send('#daily')
+  }
+});
+
+
+bot18.on('message', message => {
+  if(message.content.startsWith(`f-r`)){
+    message.channel.send(`#rep <@505101712839671838>`)
+  }
+});
+
+
+bot18.on('ready', () => {
+  let ch = bot18.guilds.get("507972785650401301").channels.find(c => c.id == "515838588299247626");
+  setInterval(function() {
+    ch.send("18th Lover")
+  }, 900);
+});
+
+
+bot18.on('message', message => {
+  let command = message.content.split(" ")[0];
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.reply(`شو يدك امي تقول؟؟؟`);
+  if(command == "f3"){
+    message.channel.send(args.join(" "))
+  }
+});
+
+
+bot18.login(process.env.FB18);
